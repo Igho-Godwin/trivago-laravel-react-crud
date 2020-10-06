17bcd0bfcf8bb413b1ffb9829362e32ca32ea292
unknown <ighogodwinavweroswo@gmail.com>
2020-10-06 02:25:35 +0100

    adding throttle and frontend configuration

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ItemController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -18,9 +19,11 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/items/all', [ItemController::class, 'getAll']);
-Route::get('/item/{id}',[ItemController::class, 'get']);
-Route::post('/item/create',[ItemController::class, 'create']);
-Route::put('/item/update/{id}',[ItemController::class, 'update']);
-Route::delete('/item/delete/{id}',[ItemController::class, 'delete']);
-Route::patch('/item/book/{id}',[ItemController::class, 'book']);
+Route::middleware('throttle:600,1')->group(function () {
+    Route::get('/items/all', [ItemController::class, 'getAll']);
+    Route::get('/item/{id}', [ItemController::class, 'get']);
+    Route::post('/item/create', [ItemController::class, 'create']);
+    Route::put('/item/update/{id}', [ItemController::class, 'update']);
+    Route::delete('/item/delete/{id}', [ItemController::class, 'delete']);
+    Route::patch('/item/book/{id}', [ItemController::class, 'book']);
+});
